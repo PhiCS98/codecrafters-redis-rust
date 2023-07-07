6f019eee24f1e17ac97f6b183cf337842ced0dff
PhiCS98 <philipptolksdorf.pt@gmail.com>
2023-07-07 17:17:27 +0200

    implemented stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{net::TcpListener, io::Write};
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,8 +10,10 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+
+                _stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
